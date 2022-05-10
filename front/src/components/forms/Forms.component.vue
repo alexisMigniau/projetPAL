62191cec7d69bef70c3ff281cdf7c4a7483f1b78
Alexis__GG <alexisgregoire2899@gmail.com>
2022-05-10 19:52:55 +0200

    Add forms panel for desktop

--- front/src/components/forms/Forms.component.vue
+++ front/src/components/forms/Forms.component.vue
@@ -3,11 +3,30 @@
         class="forms"
         v-bind:class="{ forms_mobile: isMobileDevice }"
     >
+        <div
+            class="forms__panel"
+            v-bind:class="{  forms__panel_hidden: doDisplayFormPanel }"
+        >
+            <div
+                class="forms__panel__close"
+                v-on:click="setDisplayedForm('')"
+            >
+                <img src="../../assets/icons/cross.svg" alt="Close"/>
+            </div>
+        </div>
         <div class="forms__buttons">
-            <div class="forms__buttons__btn">
+            <div
+                class="forms__buttons__btn"
+                v-bind:class="{ forms__buttons__btn_selected: isSelected('search') }"
+                v-on:click="setDisplayedForm('search')"
+            >
                 <img src="../../assets/icons/search.svg" alt="Search"/>
             </div>
-            <div class="forms__buttons__btn">
+            <div
+                class="forms__buttons__btn"
+                v-bind:class="{ forms__buttons__btn_selected: isSelected('add') }"
+                v-on:click="setDisplayedForm('add')"
+            >
                 <img src="../../assets/icons/add.svg" alt="Add"/>
             </div>
         </div>
@@ -17,7 +36,23 @@
 <script>
 export default {
     name: "Forms",
-    props: ["isMobileDevice"]
+    props: ["isMobileDevice"],
+    data: () => ({
+        displayedForm: ""
+    }),
+    methods: {
+        setDisplayedForm(form) {
+            this.displayedForm = form
+        },
+        isSelected(form) {
+            return this.displayedForm === form
+        }
+    },
+    computed: {
+        doDisplayFormPanel() {
+            return this.displayedForm.length === 0
+        }
+    }
 };
 </script>
 
