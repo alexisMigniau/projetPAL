7398f36901f84e2c062d59c60c4b9ddc9e445b96
AlexisPortable <migniau.alexis@gmail.com>
2022-05-11 16:45:11 +0200

    Changement couleur en fonction de la valeur marked

--- front/src/components/map/Map.component.vue
+++ front/src/components/map/Map.component.vue
@@ -20,19 +20,37 @@
 
 <script>
     import {LMap, LTileLayer, LMarker, LGeoJson} from 'vue2-leaflet';
-    import { Icon, latLng } from 'leaflet';
+    import L from 'leaflet';
     import 'leaflet/dist/leaflet.css';
     import { getPanneaux } from '@/services/api/panneaux'
     import { getCirconscriptions } from '@/services/api/circonscriptions'
 
     import isMobileDevice from "@/helpers/device"
 
-    delete Icon.Default.prototype._getIconUrl;
-    Icon.Default.mergeOptions({
-        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
         shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
     });
+
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    var redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
 
     export default {
         name: "Map",
@@ -64,6 +82,12 @@
 
                         if(!isMobileDevice())
                             layer.bindPopup(content)
+                    },
+                    pointToLayer : (feature, latlng) => {
+                        if(feature.properties.marked)
+                            return new L.Marker(latlng, { icon : greenIcon })
+                        else
+                            return new L.Marker(latlng, { icon : redIcon })
                     }
                 }
             },
@@ -76,11 +100,11 @@
                 url: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1pZ25pYXUiLCJhIjoiY2wyb2cxbHgxMWphYTNlbDEzcTN0YzFiaCJ9.9rjrwcyVVoAuVmbsIv3Sdw',
                 attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                 zoom: 9,
-                location : latLng(47.478419, -0.563166),
+                location : L.latLng(47.478419, -0.563166),
                 map : null,
                 center : [47.478419, -0.563166],
                 panneaux : null,
-                circonscriptions : null
+                circonscriptions : null,
             };
         },
         methods : {
@@ -95,7 +119,7 @@
                 if(("geolocation" in navigator)) {
                     this.gettingLocation = true;
                     navigator.geolocation.getCurrentPosition(pos => {
-                        this.location = latLng(pos.coords.latitude, pos.coords.longitude)
+                        this.location = L.latLng(pos.coords.latitude, pos.coords.longitude)
                         this.center = [ pos.coords.latitude,pos.coords.longitude]
                     })
                 }
